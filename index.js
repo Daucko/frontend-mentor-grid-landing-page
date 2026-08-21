021359a672239d4dcf889ea422ab62b476aaafd7
Daucko <dauda.a.tijani@gmail.com>
2026-08-21 14:03:32 +0100

    Added mobile screen responsiveness

---
+++ index.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+  const sidebar = document.querySelector('.menu');
+  sidebar.style.display = 'grid';
+}
+
+function hideSidebar() {
+  document.querySelector('.menu').style.display = 'none';
+}
